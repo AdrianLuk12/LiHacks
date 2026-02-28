1e562b14ade253454f4ede8aeebdc115f393e514
Bryant Ruan <131702861+bruan5483@users.noreply.github.com>
2026-02-28 14:53:24 -0500

    Supress browser extension warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="antialiased min-h-screen">{children}</body>
+      <body className="antialiased min-h-screen" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
